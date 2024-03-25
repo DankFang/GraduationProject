eb348391efd9393bf0deb826f178b077d15b8219
DankFang <1975504764@qq.com>
2024-03-26 02:11:21 +0800

    研究状态变量AddressZero, MaxUint256的导入

--- ProjectContracts/deploy/02_Account.js
+++ ProjectContracts/deploy/02_Account.js
@@ -2,7 +2,9 @@ const {ethers} = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 const { init, getContract, getAccount } = require("hardtron")
 const accounts = require("../config/accounts");
-
+const { AddressZero, MaxUint256 } = require("@ethersproject/constants");
+// ethers6语法
+// const {ZeroAddress, MaxUint256} = ethers
 module.exports = async ({getNamedAccounts, deployments, getChainId}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
