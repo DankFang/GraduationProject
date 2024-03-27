7860a6e0438c1a105aa25ff8ada1d3c8ca9eff73
DankFang <1975504764@qq.com>
2024-03-27 20:04:57 +0800

    upadte

--- ProjectContracts/config/network.js
+++ ProjectContracts/config/network.js
@@ -1,5 +1,4 @@
-// const ALCHEMY_API_KEY_Sepolia = "DOTlZeIL5aDxL-UFsz29v1MvatC-OY2e"
-const PRIVATE_KEY = "0b6243f3d9d8b69ede6c341ae30b27414a0a76b921471cce3b6b8a4fb6ebc805";
+
 module.exports = {
     hardhat: {
       // deploy: ['compound-deploy/'],
@@ -9,7 +8,6 @@ module.exports = {
       }
     },
     Sepolia: {
-      // url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_Sepolia}`,
       url: process.env.ALCHEMY_API_KEY_Sepolia,
       accounts: [process.env.PRIVATE_KEY]
     },
